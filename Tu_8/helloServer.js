7162171fc198df92af125399f52def426a215b09
Fitsum Alemu <fitsumcs@gmail.com>
2020-07-09 16:41:01 -0400

    Adding html content type

--- Tu_8/helloServer.js
+++ Tu_8/helloServer.js
@@ -1,10 +1,17 @@
 const http = require('http');
 
 
+// const server = http.createServer(
+//     (req,res)=>{
+//         res.writeHead(200,{'Content-Type':'text/plain'});
+//         res.end('Hello World This is My First Server..!!!');
+        
+//     }
+// );
 const server = http.createServer(
     (req,res)=>{
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end('Hello World This is My First Server..!!!');
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end('<h1>Hello World This is My First Server..!!!</h1>');
         
     }
 );
