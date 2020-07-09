788d9c14e5ccfe2f85b37c6477362c2f8725932f
Fitsum Alemu <fitsumcs@gmail.com>
2020-07-09 17:19:17 -0400

    Adding some text on incoming request

--- Tu_9/app.js
+++ Tu_9/app.js
@@ -6,6 +6,7 @@ const server = http.createServer((req,res)=>{
     fs.readFile(`${__dirname}/index.html`,(err,data)=>{
         res.writeHead(200,{'Content-Type':'text/html'});
         res.write(data);
+        console.log(`....Incoming Request : ${req.url}`);
         res.end();
 
     })
