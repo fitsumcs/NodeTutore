aa2aa8a127904f4744686c1dba1bfe79c18148cb
Fitsum Alemu <fitsumcs@gmail.com>
2020-07-09 15:54:21 -0400

    Testing the WriteStream

---
+++ Tu_6/wrtiteStream.js
@@ -0,0 +1,24 @@
+// writeable stream 
+
+const fs = require("fs");
+
+const writeStream = fs.createWriteStream(`${__dirname}/assets/myfile.txt`,'UTF-8');
+const readStream = fs.createReadStream(`${__dirname}/assets/read.md`,'UTF-8')
+// writeStream.write("Hello ..\n");
+// writeStream.write("World ..\n");
+
+// pip from readable stream to writeable stream 
+// process.stdin.on("data",data=>{
+//     writeStream.write(data);
+// });
+  
+//   With pipe method 
+// process.stdin.pipe(writeStream);
+
+
+// pip from readable stream to writeable stream 
+// readStream.on("data",data=>{
+//     writeStream.write(data);
+// });
+
+readStream.pipe(writeStream);
