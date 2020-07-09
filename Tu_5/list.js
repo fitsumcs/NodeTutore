2444153e237252bb5377d71772eae45de30c0a8b
Fitsum Alemu <fitsumcs@gmail.com>
2020-07-09 11:29:15 -0400

    Working with File System Module (Async)

--- Tu_5/list.js
+++ Tu_5/list.js
@@ -6,11 +6,18 @@ const path = require("path");
 const t = path.join(__dirname,"files");
 // read contenet of dir 
 // Sync or Async 
-console.log("Started Reading files ...")
-const files = fs.readdirSync(t);  
-console.log("completed reading ..")
+// console.log("Started Reading files ...")
+// const files = fs.readdirSync(t);  
+// console.log("completed reading ..")
+
+// console.log(files);
+
 // async 
-
-
-console.log(files);
+console.log("Started Reading files ...")
+const files = fs.readdir(t,(err,files)=>{
+    console.log("completed reading ..");
+    
+    console.log(files);
+});  
+console.log("On Progress...")
 
