7ee6b036722a7023650e51c82ad9c015270aeb04
3020 <fitsumcs@gmail.com>
2020-07-18 14:12:29 +0300

    Setting the commands [add, remove, view and all]

--- Tu_A4(Book Catalog App)/app.js
+++ Tu_A4(Book Catalog App)/app.js
@@ -4,6 +4,43 @@ const yargs = require('yargs');
 // custome yargs version
 yargs.version('1.1.0');
 
+// setting commands 
 
+//  Adding New Book 
+yargs.command({
+    command: 'add',
+    describe: 'Add New Book',
+    handler() {
+        console.log("Adding New Book");
+    }
+});
+
+// Remove new Book
+yargs.command({
+    command: 'remove',
+    describe: 'remove New Book',
+    handler() {
+        console.log("Remove New Book");
+    }
+});
+
+
+// view single book
+yargs.command({
+    command: 'view',
+    describe: 'view a Single Book',
+    handler() {
+        console.log("View Single Book");
+    }
+});
+
+// Display all books 
+yargs.command({
+    command: 'all',
+    describe: 'View All Available Books',
+    handler() {
+        console.log("View all Books");
+    }
+});
 
 console.log(yargs.argv);
