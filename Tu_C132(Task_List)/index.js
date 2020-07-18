141c51a4dd5aa4ced6893f744f3f071d3dd859c3
3020 <fitsumcs@gmail.com>
2020-07-18 13:22:06 +0300

    Adding icon on task list app with static file use in express

--- Tu_C132(Task_List)/index.js
+++ Tu_C132(Task_List)/index.js
@@ -14,10 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-console.log(express.static('public'));
-
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 // routes 
 app.get('/', (req, res) => {
