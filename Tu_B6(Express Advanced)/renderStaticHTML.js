3074b257315c7036e79f55699fe379748369d543
3020 <fitsumcs@gmail.com>
2020-07-18 19:07:42 +0300

    Serving HTML file with express sendFile

--- Tu_B6(Express Advanced)/renderStaticHTML.js
+++ Tu_B6(Express Advanced)/renderStaticHTML.js
@@ -1,21 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // The Root page with get request // we can use also /
 app.get('', (req, res) => {
-    res.send('Hello Express !!');
-});
-// help about 
-app.get('/about', (req, res) => {
-    res.send('This is About page :)');
-});
-// help route 
-app.get('/help', (req, res) => {
-    res.send('This is Help page :)');
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    // res.send('hi');
+
 });
 
+console.log(__dirname);
 
 app.listen(port, () => {
     console.log(
