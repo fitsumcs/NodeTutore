2be3099fecea0ed5f3b8ffcd8b991ca6e1126626
3020 <fitsumcs@gmail.com>
2020-07-20 17:44:44 +0300

    Working with mongodb connections

---
+++ Tu_B9(Mongo Intro)/mongoIntro.js
@@ -0,0 +1,18 @@
+// CRUD on Mongo DB
+
+// Require mongodb native driver
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+// Db info 
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'users-info';
+
+// connecting to db in Async
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to DB");
+    }
+    console.log('Databse Connected......');
+
+});
