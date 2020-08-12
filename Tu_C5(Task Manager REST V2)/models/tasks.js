66f6a34165f926826f07ba9c022025a055ee14ce
3020 <fitsumcs@gmail.com>
2020-08-12 18:19:36 +0300

    Adding reference of User Model on creater field

--- Tu_C5(Task Manager REST V2)/models/tasks.js
+++ Tu_C5(Task Manager REST V2)/models/tasks.js
@@ -15,7 +15,8 @@ const taskSchema = new mongoose.Schema({
     },
     creater: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref: 'User'
     }
 });
 
