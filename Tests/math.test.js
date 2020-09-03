3c9f73e08112a11e288195e5b5d250d03e1afb6d
3020 <fitsumcs@gmail.com>
2020-09-03 15:38:05 +0300

    Testing with jest demo

---
+++ Tests/math.test.js
@@ -0,0 +1,17 @@
+test('Hello world', () => {});
+
+test('Hello world', () => {
+    throw new Error();
+});
+
+// why testing 
+/*  
+1. Saves time 
+2. Create reliable software 
+3. Gives flexiblitiy to developers
+    => Refactoring
+    => Collaborating 
+    => Profiling 
+4. Peace of mind  
+
+*/
