f450b821620821522bcc89c19633a82562ce4a8d
3020 <fitsumcs@gmail.com>
2020-09-23 12:45:31 +0300

    check image url test modified

--- Tu_A3(Node_NPM_Modules)/8_checkImageurl.js
+++ Tu_A3(Node_NPM_Modules)/8_checkImageurl.js
@@ -1,16 +1,27 @@
 const isImageUrl = require('is-image-url');
+const url1 = "https://images.unsplash.com/photo-1600678217111-a23a045721e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+const url2 = 'https://timedotcom.files.wordpress.com/2015/08/gettyimages-482708894.jpg?quality=65&strip=color&w=1100';
 
-const result1 = isImageUrl('https://timedotcom.files.wordpress.com/2015/08/gettyimages-482708894.jpg?quality=65&strip=color&w=1100');
-//=> true
+function check_image1(url) {
+    const result1 = isImageUrl(url1);
+    console.log(result1);
+}
 
-const result2 = isImageUrl('https://images.unsplash.com/photo-1600678217111-a23a045721e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
-//=> false
-
-console.log(result1);
-console.log(result2);
 
 const requestImageSize = require('request-image-size');
 
-requestImageSize('https://images.unsplash.com/photo-1600678217111-a23a045721e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
-    .then(size => console.log(size))
-    .catch(err => console.error(err));
+async function check_image2(url) {
+
+    try {
+        const imageurl = await requestImageSize(url);
+        console.log(imageurl);
+
+    } catch (error) {
+
+        console.log("Not Image");
+
+    }
+
+}
+//check_image1(url1);
+check_image2(url2);
