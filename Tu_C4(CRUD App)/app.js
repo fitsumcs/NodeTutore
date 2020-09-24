aa1ff597c9a9e788078f80c45db2ff827c0d4418
3020 <fitsumcs@gmail.com>
2020-09-24 15:54:49 +0300

    Finishing the app ui for CRUD app

--- Tu_C4(CRUD App)/app.js
+++ Tu_C4(CRUD App)/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
-const PORT = process.env.PORT || 3000
+const path = require('path');
+const hbs = require('hbs');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// view 
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/partials'));
+
 app.get('/', (req, res) => {
-    res.send("Hello Man !!");
+    res.render("index");
+});
+app.get('/about', (req, res) => {
+    res.render("about");
 });
 
 app.listen(PORT, () => console.log("Server Started!"));
