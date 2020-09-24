8cbf4ff423a5cfc48f78e04f5ef8bc324b714ee6
3020 <fitsumcs@gmail.com>
2020-09-24 19:27:03 +0300

    decomposing routes

--- Tu_C8(CRUD App V2 [with auth])/app.js
+++ Tu_C8(CRUD App V2 [with auth])/app.js
@@ -6,8 +6,11 @@ const body_parser = require('body-parser');
 const methodOverride = require('method-override');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
-const Note = require('./models/note');
 
+
+// routes
+const noteRouter = require('./routes/notes');
+const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 3000;
 
@@ -46,87 +49,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render("about");
 });
-// get all notes 
-app.get('/allnotes', (req, res) => {
-    Note.find({}).sort({ date: 'desc' }).exec((err, notes) => {
-        if (err) {
-            return console.log(err);
-        }
-        res.render('allnotes', { notes });
-    });
 
-});
-// Notes form
-app.get('/notes', (req, res) => {
-    res.render("new_notes");
-});
-app.post('/notes', (req, res) => {
-
-    const errors = [];
-    if (!req.body.title) {
-        errors.push({ text: "Please Enter Title!!" });
-    }
-    if (!req.body.description) {
-        errors.push({ text: "Please Enter Description!!" });
-    }
-    if (errors.length > 0) {
-        res.render("new_notes", {
-            errors,
-            title: req.body.title,
-            description: req.body.description
-        });
-    } else {
-        const newNote = new Note({
-            title: req.body.title,
-            description: req.body.description
-        });
-        newNote.save((err, note) => {
-            if (err) {
-                return console.log(err);
-            }
-            req.flash('success_msg', "Note Added !!");
-            res.redirect('/allnotes');
-        });
-
-    }
-});
-
-// edit 
-app.get('/notes/:id', (req, res) => {
-    Note.findById(req.params.id, (err, note) => {
-        if (err) {
-            return console.log(err);
-        }
-        res.render("edit_notes", {
-            note
-        });
-    });
-
-});
-//edit
-app.put('/notes/:id', (req, res) => {
-    const note = {
-        title: req.body.title,
-        description: req.body.description
-    };
-    Note.findOneAndUpdate({ _id: req.params.id }, note, (err, note) => {
-        if (err) {
-            return console.log(err);
-        }
-
-        res.redirect('/allnotes');
-    });
-
-});
-// delete
-app.delete('/notes/:id', (req, res) => {
-
-    Note.deleteOne({ _id: req.params.id }, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        req.flash('success_msg', "Note Deleted !!");
-        res.redirect('/allnotes');
-    });
-});
+app.use(noteRouter);
+app.use(authRouter);
 app.listen(PORT, () => console.log("Server Started!"));
---
+++ Tu_C8(CRUD App V2 [with auth])/routes/notes.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const Note = require('../models/note');
+const router = express.Router();
+
+
+// get all notes 
+router.get('/allnotes', (req, res) => {
+    Note.find({}).sort({ date: 'desc' }).exec((err, notes) => {
+        if (err) {
+            return console.log(err);
+        }
+        res.render('allnotes', { notes });
+    });
+
+});
+// Notes form
+router.get('/notes', (req, res) => {
+    res.render("new_notes");
+});
+router.post('/notes', (req, res) => {
+
+    const errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "Please Enter Title!!" });
+    }
+    if (!req.body.description) {
+        errors.push({ text: "Please Enter Description!!" });
+    }
+    if (errors.length > 0) {
+        res.render("new_notes", {
+            errors,
+            title: req.body.title,
+            description: req.body.description
+        });
+    } else {
+        const newNote = new Note({
+            title: req.body.title,
+            description: req.body.description
+        });
+        newNote.save((err, note) => {
+            if (err) {
+                return console.log(err);
+            }
+            req.flash('success_msg', "Note Added !!");
+            res.redirect('/allnotes');
+        });
+
+    }
+});
+
+// edit 
+router.get('/notes/:id', (req, res) => {
+    Note.findById(req.params.id, (err, note) => {
+        if (err) {
+            return console.log(err);
+        }
+        res.render("edit_notes", {
+            note
+        });
+    });
+
+});
+//edit
+router.put('/notes/:id', (req, res) => {
+    const note = {
+        title: req.body.title,
+        description: req.body.description
+    };
+    Note.findOneAndUpdate({ _id: req.params.id }, note, (err, note) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        res.redirect('/allnotes');
+    });
+
+});
+// delete
+router.delete('/notes/:id', (req, res) => {
+
+    Note.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        req.flash('success_msg', "Note Deleted !!");
+        res.redirect('/allnotes');
+    });
+});
+
+
+module.exports = router;
