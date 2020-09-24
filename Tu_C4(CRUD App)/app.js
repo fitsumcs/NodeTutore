b77367ada2c5d7d36fdcedfc4650ef3dbfc89893
3020 <fitsumcs@gmail.com>
2020-09-24 16:53:04 +0300

    handling post route with sticky form

--- Tu_C4(CRUD App)/app.js
+++ Tu_C4(CRUD App)/app.js
@@ -1,19 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const hbs = require('hbs');
+const bodyparser = require('body-parser');
+const Note = require('./models/note');
+
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//database 
+mongoose.connect('mongodb://localhost/note_taker', { useUnifiedTopology: true, useNewUrlParser: true });
+
+// middleware config 
+app.use(bodyparser.urlencoded({ extended: false }));
 // view 
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname, '/partials'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res) => {
     res.render("index");
 });
 app.get('/about', (req, res) => {
     res.render("about");
+});
+
+// Notes form
+app.get('/notes', (req, res) => {
+    res.render("new_notes");
+});
+app.post('/notes', (req, res) => {
+
+    const errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "Pleas Enter Title!!" });
+    }
+    if (!req.body.description) {
+        errors.push({ text: "Pleas Enter Description!!" });
+    }
+    if (errors.length > 0) {
+        res.render("new_notes", {
+            errors,
+            title: req.body.title,
+            description: req.body.description
+        });
+    } else {
+        res.redirect('/');
+    }
+
+
 });
 
 app.listen(PORT, () => console.log("Server Started!"));
